3e2b0eb977a8ebcbdd8f225d4dc607bc47a1be3e
Crypt00o <octopus2672001@gmail.com>
2022-09-28 10:54:50 +0200

    Adding struct and enums practice files

---
+++ enums.rs
@@ -0,0 +1,160 @@
+#![allow(warnings)]
+
+enum Animal {
+    Cat,
+    Dog,
+    Snake,
+    Ferris,
+}
+
+impl Animal {
+    fn is_equal_to(&self, other: &Self) -> bool {
+        match self {
+            &Self::Cat => match other {
+                &Self::Cat => return true,
+                _ => return false,
+            },
+            &Self::Dog => match other {
+                &Self::Dog => return true,
+                _ => return false,
+            },
+            &Self::Snake => match other {
+                &Self::Snake => return true,
+                _ => return false,
+            },
+            &Self::Ferris => match other {
+                &Self::Ferris => return true,
+                _ => return false,
+            },
+            _ => return false,
+        }
+    }
+}
+
+enum Person {
+    NormalPerson {
+        name: String,
+        age: u8,
+    },
+    Developer {
+        name: String,
+        age: u8,
+        language: String,
+    },
+    Hacker {
+        name: String,
+        age: u8,
+        language: String,
+        skills: String,
+    },
+}
+
+impl Person {
+    fn info(&self) {
+        match self {
+            Self::Hacker {
+                name,
+                language,
+                skills,
+                age,
+            } => {
+                println!("\n Type Hacker : \nname : {name} , age : {age}, fav languages : {language} ,skills : {skills} ")
+            }
+            Self::NormalPerson { name, age } => {
+                println!("\n Type NormalPerson : \nname : {name},age : {age}")
+            }
+            Self::Developer {
+                name,
+                age,
+                language,
+            } => {
+                println!(
+                    "\n Type Developer : \nname : {name}, age : {age} , languages  : {language} "
+                )
+            }
+            _ => {
+                println!("default")
+            }
+        }
+    }
+
+    fn init_hacker(name: &str, age: u8, language: &str, skills: &str) -> Person {
+        return Self::Hacker {
+            name: String::from(name),
+            age,
+            language: String::from(language),
+            skills: String::from(skills),
+        };
+    }
+
+    fn init_normal_person(name: &str, age: u8) -> Person {
+        return Self::NormalPerson {
+            name: String::from(name),
+            age,
+        };
+    }
+
+    fn init_developer(name: &str, age: u8, language: &str) -> Person {
+        return Self::Developer {
+            name: String::from(name),
+            age,
+            language: String::from(language),
+        };
+    }
+
+    fn clear(&mut self) {
+        match self {
+            Self::Hacker {
+                name,
+                age,
+                language,
+                skills,
+            } => {
+                name.clear();
+                *age = 0;
+                language.clear();
+                skills.clear()
+            }
+            Self::NormalPerson { name, age } => {
+                name.clear();
+                *age = 0
+            }
+            Self::Developer {
+                name,
+                age,
+                language,
+            } => {
+                name.clear();
+                language.clear();
+                *age = 0
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut me: Person = Person::init_hacker(
+        "0xCrypt00o",
+        21,
+        "assembly 0x86 , assembly 0x86_64 , C , Python3 , Js  ",
+        "Network , Os Internals(Unix,NT) Exploitation with manual written assembly shellcodes ",
+    );
+    me.info();
+    me.clear();
+    me = Person::init_developer(
+        "eslam mohamed",
+        21,
+        "Js , Skills(NodeJs , ExpressJs , MySQl , Postgresql , MongoDB) ",
+    );
+    me.info();
+    me.clear();
+    me = Person::init_normal_person("eslam", 21);
+    me.info();
+    me.clear();
+
+    let cat: Animal = Animal::Cat;
+    let dog: Animal = Animal::Dog;
+    let cat2: Animal = Animal::Cat;
+    println!("\nis cat2 equal to cat ?  {} ", cat2.is_equal_to(&cat));
+    println!("\nis cat  equal to dog ?  {} ", cat.is_equal_to(&dog));
+}
---
+++ struct.rs
@@ -0,0 +1,55 @@
+#![allow(warnings)]
+
+struct Person {
+    name: String,
+    age: u8,
+}
+impl Person {
+    
+    fn new() -> Self {
+        return Self {
+            name: String::new(),
+            age: 0,
+        };
+    }
+
+    fn init(name: &str, age: u8) -> Self {
+        return Self {
+            name: String::from(name),
+            age,
+        };
+    }
+
+    fn clone(&self) -> Self {
+        return Self {
+            name: self.name.clone(),
+            age: self.age,
+        };
+    }
+
+    fn clear(&mut self) {
+        *self = Person::new();
+    }
+
+    fn is_equal_to(&self,other:&Self)->bool{
+        if self.name.as_str() == other.name.as_str() && self.age == other.age{
+            return true
+        }
+        else{
+            return false;
+        }
+    }
+
+    fn info(&self) {
+        println!("name is : {} , age is : {} ", self.name, self.age)
+    }
+}
+
+fn main() {
+    let person: Person = Person::init("eslam", 20);
+    let mut person2:Person= person.clone();
+    println!("is the 2 persons are equals ? : {}",person2.is_equal_to(&person));
+    person2.info();
+    person2.clear();
+    person2.info();
+}
