aa7ac3dc79d557ef6d475583e5def73f6c2a278d
Crypt00o <octopus2672001@gmail.com>
2022-10-11 12:27:29 +0200

    Update Optionals.rs

--- optionals.rs
+++ optionals.rs
@@ -35,7 +35,7 @@ fn main(){
       println!( "this is my name : {} ",none_name.expect("i expect string slice")); 
 
 
-    //2) with option.unwarp()
+    //2) with option.unwarp() , force unwraping unsafely
     
     println!( "this is my name : {} ",none_name.unwrap()); //this will  through a panic because none_name option =None mean it hasn,t any value 
 
@@ -52,8 +52,50 @@ fn main(){
     println!("{}",my_age.unwrap());
     
     
-    
+   // multi compareing options useing if let operation & tuples:
 
+
+    let first_name:Option<&str>=Some("eslam");
+    let middle_name:Option<&str>=Some("mohamed");
+    let last_name:Option<&str>=Some("elabd");
+
+    if let (Some(first_name_value),Some(middle_name_value),Some(last_name_value))=(first_name,middle_name,last_name){
+        println!("my name is : {first_name_value} {middle_name_value} {last_name_value}");
+    }
+    
+    // unwarping with default value useing unwarp_or(default_value) , if an Option hasn,t no value it will be equal to default value
+
+        
+    println!("myname is {}",first_name.unwrap_or("eslam"));
+
+
+    // unwarping with default function useing unwarp_or_else(callback function) 
+
+
+    println!("my name is {}",none_name.unwrap_or_else(||->&str{println!("there is no value , we will assign to default value ");return "eslam";}));
+
+
+   //unwarping with rust default_value for data type unwrap_or_default()
+
+    let age :Option<u8>=None;
+    println!("my age is {}",age.unwrap_or_default()); // age.unwrap_or_default() = 0 
+
+
+    // is_some(&self)->bool or is_none(&self)->bool to check if a option has a value or not 
+
+        if first_name.is_some(){
+            println!("first_name has a value")
+        }
+
+        if none_name.is_none(){
+            println!("last_name_value");
+        }
+    
+        //mapping an option usein map(&self , function )->Option
+        
+
+        let age : Option<u8>= Some(10);
+        println!("my age is {} ", age.map(|x|x*2).unwrap_or_default() );
 
 
 }
