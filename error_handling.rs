5090cc161e3627a0fe3314728d43f467b8c62509
Crypt00o <octopus2672001@gmail.com>
2022-10-12 19:55:58 +0200

    add error_handling.rs

---
+++ error_handling.rs
@@ -0,0 +1,48 @@
+//Result Type Contain Ok() or Err() some like Option ,Some(),None
+//Result<T,E> mean : Result<value_type,error_type>
+
+fn main(){
+   //declareing an Result<valuetype,errtype>
+
+    let mut some_result:Result<&str,&str>=Ok("value");
+     
+    // warping the result with matching 
+
+    match some_result {
+        Ok(value)=>{println!("value : {}",value)},
+        Err(error)=>{println!("error happened : {}",error)}
+    }
+    
+
+     some_result=Err("runtime error");
+    
+     match some_result {
+        Ok(value)=>{println!("value : {}",value)},
+        Err(error)=>{println!("error happened : {}",error)}
+    }
+    
+
+    // expect(&self,msg:&str) warping result and if Error , then Exit or panic  with a runtime error in thread  with error type and error message
+
+
+     some_result.expect("expecting no error");
+    
+
+    // expect_err (&self,msg:&str) warping result and if Ok and not  Err , then Exit or panic with a runtime error in thread  with expect message
+
+    some_result.expect_err("expecting error");
+    
+    // is_ok(&self)->bool return true if  result return Ok(value);
+
+    if some_result.is_ok(){
+        println!("it,s ok ");
+    }
+
+    // is_err(&self)->bool return true if  result return Err(error);
+
+    if some_result.is_err(){
+        println!("no error");
+    }
+
+
+}
